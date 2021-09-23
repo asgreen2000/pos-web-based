98e1442e05525d1a68f6617a92a7d9bf8a49b908
Bùi Hữu Tiến Đạt <dat.bui197404648@hcmut.edu.vn>
2021-09-23 11:49:23 +0700

    Add

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname + 'public'));
+
+const url = "mongodb+srv://admin-asgreen:0353297204@cluster0.aywbz.mongodb.net/foodDB";
+
+mongoose.connect(url);
+
+const foodSchema = mongoose.Schema({
+
+    name: String, 
+    url: String,
+    cost: Number,
+    type: String
+});
+
+const Food = mongoose.model('Food', foodSchema);
+
+
+app.get('/', function(req, res) {
+    Food.find({}, (err, result) => {
+        res.sendFile('index.html', {root: __dirname })
+    })
+});
+
+app.post('/submit' , (req , res)=>{
+
+    const name = req.body.name;
+    const url = req.body.url;
+    const cost = req.body.cost;
+    let food = new Food(
+        {
+            name: name,
+            url: url,
+            cost: cost
+        });
+    food.save().then(
+        res.redirect('/')
+    )
+});
+
+app.listen(process.env.PORT, () => console.log("The server ran!"));
